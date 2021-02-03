b1800622e4f5b4d93d7cebf5e6f46d4b4b7ea62e
mischadab <mischadabrowski@gmail.com>
2021-02-02 19:39:01 -0500

    added to js file

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 // take search button into variable (API KEY)
 // function ( input )
 
-
-
-function citySearch(){
-    event.preventDefault();
-    var cityNames = document.querySelector('#enteredInfo').nodeValue
-    var storage = localStorage.setItem("info", JSON.stringify(cityNames));
+function searchCity(city){
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "b13426899baf1ab4f2b674c71d680b79";
 }
 
-function renderSearch(){
-    var cityInfo = JSON.parse(localStorage.getItem("info"));
+
+
+
+
+
+
+
+
+
+
+
+// function citySearch(){
+//     event.preventDefault();
+//     var cityNames = document.querySelector('#enteredInfo').nodeValue
+//     var storage = localStorage.setItem("info", JSON.stringify(cityNames));
+// }
+
+// function renderSearch(){
+//     var cityInfo = JSON.parse(localStorage.getItem("info"));
     // document.querySelector("").textContent += 
-}
+// }
