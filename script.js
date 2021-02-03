3a9b71c6ff462b08c67ef9008bc04a29a9977236
mischadab <mischadabrowski@gmail.com>
2021-02-03 17:05:44 -0500

    added css styling and fetch weather js

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-// take search button into variable (API KEY)
-// function ( input )
+// search city function
+async function searchCity(city){
+    var APIkey = "b13426899baf1ab4f2b674c71d680b79";
+    var urlWeather = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
+    var urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${cityVar}&appid=${APIkey}`;
 
-function searchCity(city){
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "b13426899baf1ab4f2b674c71d680b79";
+ // fetch info based on location entered
+    await fetch(urlWeather).then(function (response){
+        return response.json();
+    })
+    .then(function (data) {
+        var result = document.querySelector("location");
+        var Kelvin = `${data.main.temp}`;
+        var celcius = Math.round(Kelvin - 273.15);
+        var currentDate = moment().format("l");
+        result.innerHTML = `
+        <div class="card" style="width: 100%;">
+        <h3 class="card-title">${
+          data.name
+        } (${currentDate}) ${`<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>`}</h3>
+            <ul class="cardUl">
+                <li>Temperature: ${celsius} °C</li>
+                <br>
+                <li>Humidity: ${data.main.humidity}</li>
+                <br>
+                <li>Wind speed: ${data.wind.speed} MPH</li>
+            </ul>
+            </div>
+          </div>`
+    })
 }
 
 
