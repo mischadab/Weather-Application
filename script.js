fdd4058da334bcf75bce8b2c326c840b48a6d991
mischadab <mischadabrowski@gmail.com>
2021-02-04 17:12:29 -0500

    got js fetch function to work

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
-
-// $("#searchButton").on("click", function(){
-//     var enteredCity = $("#cityInput").val();
-//     console.log(enteredCity)
-// })
-
 // grab input from input element on click
 document.getElementById("searchButton").onclick = function() {
 var enteredCity = document.getElementById("cityInput").value
+// clearing input field
+document.getElementById("cityInput").value = ""
+searchCity(enteredCity)
 }
-
-
 
 // search city function
 async function searchCity(city){
+    
     var APIkey = "b13426899baf1ab4f2b674c71d680b79";
-    var urlWeather = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
-    var urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${cityVar}&appid=${APIkey}`;
+    var urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
+    // var urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}`;
 
  // fetch info based on location entered
-    await fetch(urlWeather).then(function (response){
-        return response.json();
-    })
-    .then(function (data) {
-        var result = document.querySelector("location");
-        var Kelvin = `${data.main.temp}`;
-        var celcius = Math.round(Kelvin - 273.15);
-        var currentDate = moment().format("l");
-        result.innerHTML
-    })
+    fetch(urlWeather).then(response => response.json())
+    .then(json = console.log (json.name))
+    .catch(err => console.log('Request failed', err));
+    // .then(function (data) {
+    //     var result = document.querySelector("location");
+    //     var Kelvin = `${data.main.temp}`;
+    //     var celcius = Math.round(Kelvin - 273.15);
+    //     var currentDate = moment().format("l");
+    //     result.innerHTML 
+    // })
 }
 
-
-
-
-
-
+// get each card for 5day forecast an id
+// how to get data from json in a fetch call
 
 
 
